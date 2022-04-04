f8d2c0011ae3fa7bdcab805a58b5790c24333a1c
ThanhTrung2001 <thanhtrungtran13@gmail.com>
2022-04-04 21:12:50 +0700

    Add Dialog Accept

--- familiar_stranger/lib/Component/Dialog/dialog_Custom_WithIcon_Button.dart
+++ familiar_stranger/lib/Component/Dialog/dialog_Custom_WithIcon_Button.dart
@@ -1,4 +1,51 @@
+import 'package:familiar_stranger/Component/Button/dialog_Button.dart';
+import 'package:familiar_stranger/constant.dart';
 import 'package:flutter/material.dart';
 
+class Dialog_AcceptInvite extends StatelessWidget {
+  final VoidCallback press_yes;
+  const Dialog_AcceptInvite({ 
+    Key? key,
+    required this.press_yes,
+    }) : super(key: key);
 
-
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      elevation: 0,
+      backgroundColor: Transp_Color,
+      child: Container(
+      height: size.height*0.35,
+      width: size.width*0.8,
+      decoration: BoxDecoration(
+        border: Border.all(width: 5.0, color: Color.fromARGB(255, 99, 53, 156)),
+        color: Color.fromARGB(255, 220, 146, 231),
+        borderRadius: BorderRadius.all(Radius.circular(5.0)),
+      ),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Image.asset('assets/Icons/Ok_Hand.png'),
+        SizedBox(height: 10,),
+        Text("Opponent found! Join room?", style: TextStyle(fontFamily: 'ZenDots', fontSize: 20, color: Main_Text),textAlign: TextAlign.center,),
+        Padding(
+          padding: EdgeInsets.only(top: 40, right: 10, left: 10, bottom: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Rounded_Border_Dialog_Button(text: "Yes", bordercolor: Main_Text, textcolor: Main_Text, press: press_yes , horizon: 20, verti: 10),
+              SizedBox(width: size.width*0.12,),
+              Rounded_Border_Dialog_Button(text: "No", bordercolor: Sub_Text, textcolor: Sub_Text, press: (){Navigator.of(context).pop();} , horizon: 23, verti: 10),
+            ],
+          ),
+          ),
+      ],
+    ),
+  ),
+    );
+  }
+}
--- familiar_stranger/lib/Component/NavBar/Navbar.dart
+++ familiar_stranger/lib/Component/NavBar/Navbar.dart
@@ -1,6 +1,7 @@
 import 'package:familiar_stranger/Component/Button/dialog_Button.dart';
 import 'package:familiar_stranger/Component/Dialog/dialog_Custom_LogOut.dart';
 import 'package:familiar_stranger/Component/Dialog/dialog_Custom_OneTextField.dart';
+import 'package:familiar_stranger/Component/Dialog/dialog_Custom_WithIcon_Button.dart';
 import 'package:familiar_stranger/Component/TextField/Login/rounded_TextField_Center.dart';
 import 'package:familiar_stranger/constant.dart';
 import 'package:flutter/material.dart';
@@ -13,49 +14,44 @@ class Nav_Bar extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Drawer(
-      
       child: Container(
         color: Color.fromARGB(255, 83, 59, 91),
         child: ListView(
           padding: EdgeInsets.zero,
           children: [
             Container(
-              child: UserAccountsDrawerHeader(
-                accountName: Text("Kodoku", style: TextStyle(color: Main_Text, fontWeight: FontWeight.bold),), 
-                accountEmail: Text("", style: TextStyle(color: Main_Text, fontWeight: FontWeight.normal),),
-                currentAccountPicture: CircleAvatar(  
-                  child: ClipOval(
-                    child: Image.network(
-                      "https://i.imgur.com/DSG7rkv.jpg",
-                      width: 90,
-                      height: 90,
-                      fit: BoxFit.cover,
-                      )
-                  ),
-                ),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  image: DecorationImage(
-                    image: NetworkImage("https://i.imgur.com/qywe4pq.jpg"),
-                    fit: BoxFit.cover,
-                    colorFilter: ColorFilter.mode(Colors.black, BlendMode.dstATop),
-                    
-                  ),
-                ),
-                ),
-            ),
+              color: Color.fromARGB(255, 83, 59, 91),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text( "FS", style: TextStyle(fontFamily: 'UncialAntiqua',fontSize: 64, color: Colors.white,),textAlign: TextAlign.center,),
+                          SizedBox(
+                            height: 10,
+                          ), 
+                          
+                        ],
+                      ),
+                      ),
+            Divider(
+              color: Main_Text,
+              thickness: 6,
+              indent: 30,
+              endIndent: 30,),
             ListTile(
-              contentPadding: EdgeInsets.only(left: 35, top: 10),
+              contentPadding: EdgeInsets.only(left: 40, top: 10),
               leading: Icon(Icons.home, color: Main_Text,),
               title: Text("Home", style: TextStyle(fontSize: 15,color: Main_Text, fontWeight: FontWeight.bold),),
               onTap: () {},
             ),
-            Divider(
-              color: Main_Text,
-              thickness: 2,
-              indent: 30,
-              endIndent: 100,
-            ),
+            // Divider(
+            //   color: Main_Text,
+            //   thickness: 2,
+            //   indent: 30,
+            //   endIndent: 100,
+            // ),
             ListTile(
               contentPadding: EdgeInsets.only(left: 35, top: 10),
               leading: Icon(Icons.person, color: Main_Text,),
@@ -68,7 +64,7 @@ class Nav_Bar extends StatelessWidget {
               leading: Icon(Icons.people, color: Main_Text,),
               title: Text("Friends", style: TextStyle(fontSize: 15,color: Main_Text, fontWeight: FontWeight.bold),),
               onTap: () {
-                showDialog(context: context, builder: (context){ return Dialog_Input_One(press_yes: (){print(UIDFriend);});});
+                showDialog(context: context, builder: (context){ return Dialog_AcceptInvite(press_yes: (){},);});
               },
                 
             ),
@@ -78,12 +74,6 @@ class Nav_Bar extends StatelessWidget {
               leading: Icon(Icons.settings, color: Main_Text,),
               title: Text("Setting", style: TextStyle(fontSize: 15,color: Main_Text, fontWeight: FontWeight.bold),),
               onTap: () => Navigator.pushNamed(context, route.setting),
-            ),
-            Divider(
-              color: Main_Text,
-              thickness: 2,
-              indent: 30,
-              endIndent: 100,
             ),
             ListTile(
               contentPadding: EdgeInsets.only(left: 35, top: 10),
@@ -96,6 +86,9 @@ class Nav_Bar extends StatelessWidget {
           ],
         ),
       ),
+    ],
+    ),
+    ),
     );
   }
 }
